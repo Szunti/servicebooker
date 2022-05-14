474e500fa98a6c1cd6c87f8b341e05cf776c8482
Szunti <Szunti@users.noreply.github.com>
2022-05-14 12:48:41 +0200

    Create project with spring initializr

---
+++ src/main/java/hu/progmasters/servicebooker/ServicebookerApplication.java
@@ -0,0 +1,13 @@
+package hu.progmasters.servicebooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicebookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicebookerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/progmasters/servicebooker/ServicebookerApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmasters.servicebooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicebookerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
