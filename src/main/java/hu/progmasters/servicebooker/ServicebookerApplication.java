da69191efc5cb7952509b6df5bf72b64861987ed
Szunti <Szunti@users.noreply.github.com>
2022-06-10 17:23:29 +0200

    Add ModelMapper dependency

--- src/main/java/hu/progmasters/servicebooker/ServicebookerApplication.java
+++ src/main/java/hu/progmasters/servicebooker/ServicebookerApplication.java
@@ -1,7 +1,10 @@
 package hu.progmasters.servicebooker;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServicebookerApplication {
@@ -10,4 +13,11 @@ public class ServicebookerApplication {
 		SpringApplication.run(ServicebookerApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration()
+				.setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
+	}
 }
