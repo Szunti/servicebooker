16caa72f666b0894d40471c5a0ce30e39a45cf62
Szunti <Szunti@users.noreply.github.com>
2022-09-06 17:13:57 +0200

    Make annotation order the same for all entities

--- src/main/java/hu/progmasters/servicebooker/domain/entity/Booking.java
+++ src/main/java/hu/progmasters/servicebooker/domain/entity/Booking.java
@@ -6,9 +6,9 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity
 @Data
 @NoArgsConstructor
+@Entity
 public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
